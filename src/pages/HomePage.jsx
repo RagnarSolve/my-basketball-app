c82271957d689757dba484e4ddcc3b41eca2de21
Cedzi-95 <cedricmasinzo@yahoo.com>
2025-03-19 13:35:28 +0100

    add style, functionality, reponsive

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -16,8 +16,11 @@ const HomePage = () => {
     return (
         <div>
             <h2>Welcome to the NBA Stats App</h2>
-            <p>Select an option from the menu to view standings, game results, or player stats.</p>
-
+            
+             <div id="homePicture">
+                 <img src="https://library.sportingnews.com/styles/crop_style_16_9_desktop/s3/2025-02/GFX-1736%20NBA%20All%20Star%202025%20FTR%20package_FTR%20%231_0.jpg?itok=62EoVz5c" alt=""  id="pic"/>
+                 <img src="https://gsp-image-cdn.wmsports.io/cms/prod/bleacher-report/getty_images/2198819600_large_image_0.jpg" alt="" id="pic"/>
+             </div>
            
             <h3>Live NBA Games</h3>
             {liveGames.length > 0 ? (
@@ -40,3 +43,5 @@ const HomePage = () => {
 };
 
 export default HomePage;
+   
+         
